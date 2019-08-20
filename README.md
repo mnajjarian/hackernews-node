fb26a2908a804d490f97c43ce641712b58592075
Mahdi Najjarian <mnajarian@gmail.com>
2019-08-20 19:59:24 +0300

    filtering and paginate

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # hackernews-node
-A GraphQL tutorial
+A tutorial from HOW TO GRAPHQL
+
+In this tutorial, I learned how to build a GraphQL server from scratch. The stack I used was based on Node.js, graphql-yoga and Prisma.
+
+
--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,4 +1,24 @@
-const feed = (root, args, context, info) =>  context.prisma.links();
+const feed = async (root, args, context, info) =>  {
+    const where = args.filter ? {
+        OR: [
+            { description_contains: args.filter },
+            { url_contains: args.filter },
+        ],
+    } : {}
+    const links = await context.prisma.links({
+        where,
+        skip: args.skip,
+        first: args.first,
+        orderBy: args.orderBy
+    });
+    const count = await context.prisma.linksConnection({
+        where,
+    }).aggregate().count()
+    return {
+        links,
+        count
+    }
+}
 
 module.exports = {
     feed,
